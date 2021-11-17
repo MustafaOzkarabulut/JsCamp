a0f5e355e75a1e10040722417cacc995432d616b
Mustafa Özkarabulut <81626732+MustafaOzkarabulut@users.noreply.github.com>
2021-11-17 18:01:18 +0300

    17.11

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 console.log("Merhaba Kodlama.io")
 
-var dolarDun = 9.20
-var dolarBugun = 9.30
+//JS Type safe değildir.
+let dolarDun = 9.20
+let dolarBugun = 9.30
 
 console.log(dolarDun)
+
+const euroDun = 11.2
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kampu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log(konutKredileri)
+console.log("<ul>")
+for (let i = 0;i<konutKredileri.length;i++) {
+    console.log("<li>"+konutKredileri[i]+"<li>")
+}
+console.log("<ul>")
+
+console.log(konutKredileri)
